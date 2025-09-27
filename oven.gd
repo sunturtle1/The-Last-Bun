271f16693c24f65a5bfd8310696503819399a3fa
Marcukaa <23d_lengyelm@nyirszikszi.hu>
2025-09-27 11:32:57 +0200

    added player animations

--- oven.gd
+++ oven.gd
@@ -1,1 +1,19 @@
 extends StaticBody2D
+@onready var timer = $Timer
+var pattyscene = preload("res://patty.tscn")
+
+func _input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("interact"):
+		place_patty()
+	pass
+
+func place_patty():
+	var patty = pattyscene.instantiate()
+	add_child(patty)
+	patty.position = Vector2(0, 0)
+	timer.start()
+
+func _on_timer_timeout() -> void:
+	print("kesz a hus")
+	
+	
--- player.gd
+++ player.gd
@@ -1,5 +1,9 @@
 extends CharacterBody2D
 @export var speed: float = 500.0
+@onready var item_label = $".."/CanvasLayer/CurrentItemLabel
+@onready var anim = $AnimatedSprite2D
+var CurrentItem = ""
+
 func _process(delta: float) -> void:
 	var move_direction := Vector2.ZERO
 	if Input.is_action_pressed("up"):
@@ -15,3 +19,7 @@ func _process(delta: float) -> void:
 	velocity = move_direction * speed
 	move_and_slide()
 	
+	if move_direction != Vector2.ZERO:
+		anim.play("default")
+	else:
+		anim.play("idle")
